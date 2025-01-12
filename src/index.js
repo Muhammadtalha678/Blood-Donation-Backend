a54fcad7652097287207dba6b714b8e5b2ac49e2
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-12 13:14:02 +0500

    update index file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import authRoutes from './routers/auth.router.js'
 import cors from 'cors'
 import dbConnect from './dbConnection/dbConnection.js'
+import sendResponse from './helper/sendResponse.js'
 
 const app = express()
 
@@ -18,22 +19,12 @@ app.get('/', (req,res) => {
 app.use('/auth',authRoutes)
 
 
-const startServer = async () => {
-    try {
-        // Step 1: Connect to the database
-        await dbConnect();
-        console.log('Database connected successfully.');
 
-        // Step 2: Start the Express server
+dbConnect()
+    .then(() => {
+        console.log('Database connected successfully.')
         app.listen(process.env.PORT, () => {
             console.log(`Server running on port ${process.env.PORT}`);
         });
-    } catch (error) {
-        // Handle any critical errors during the startup process
-        console.error('Error during application startup:', error.message);
-        process.exit(1); // Exit the process with a failure code
-    }
-};
-
-// Start the application
-startServer();
+    })
+.catch((e) => console.log(e))
