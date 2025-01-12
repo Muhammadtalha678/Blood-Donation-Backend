f9b1ac07426ad297dd987c9cfcb7e4236ad62ce5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-12 13:23:34 +0500

    remove unused values in db connection

--- src/dbConnection/dbConnection.js
+++ src/dbConnection/dbConnection.js
@@ -2,12 +2,7 @@ import mongoose from 'mongoose'
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-        )
+        await mongoose.connect(process.env.MONGO_URI)
         console.log('db connect Successfully');
         
     } catch (error) {
